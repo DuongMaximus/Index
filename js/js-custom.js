382dc59b7968f8e7c2c643c9e7545610853a1c74
DuongMaximus <100463242+DuongMaximus@users.noreply.github.com>
2022-06-03 17:06:22 +0700

    Add files via upload

---
+++ js/js-custom.js
@@ -0,0 +1,63 @@
+// JavaScript Document
+$('#fashionmen').owlCarousel({
+    loop:true,
+    margin:10,
+	dots:false,
+	nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
+$('#fashionWomen').owlCarousel({
+    loop:true,
+    margin:10,
+	dots:false,
+	nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#fashionKid').owlCarousel({
+    loop:true,
+    margin:10,
+	dots:false,
+	nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
+
+
+
+
